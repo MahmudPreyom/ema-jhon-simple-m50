9b4c972b8a3e68cdafa22ee2c193282937f3ff47
MahmudPreyom <preyom5076@gmail.com>
2023-03-28 14:54:36 +0600

    show data from cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Product from "../Product/Product";
 import './Shop.css'
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -14,10 +15,46 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id
+        for (const id in storedCart) {
+            // step 2: get the product by using id
+            const addedProduct = products.find(product => product.id === id);
+            // step 3: get quantity of the product
+            if (addedProduct) {
+                // step 4: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 5:add the added Product to the saved cart
+                savedCart.push(addedProduct);
+            }
+            // console.log( 'added Product' , addedProduct);
+        }
+        // set the cart
+        setCart(savedCart);
+    }, [products])
+
     const handleAddToCart = (product) => {
         // cart.push(product);
-        const newCart = [...cart,product];
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product doesn't exist in the cart , then set quantity = 1
+        // if exist update quantity by 1
+        const exists = cart.find(pd=>pd.id===product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart= [...cart,product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd=>pd.id !== product.id);
+            newCart = [...remaining,exists];
+        }
+
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
