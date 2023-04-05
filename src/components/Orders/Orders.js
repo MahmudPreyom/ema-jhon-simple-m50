75c1601a713ebf22eb5895832bf007cbd724dd19
MahmudPreyom <preyom5076@gmail.com>
2023-04-05 16:00:08 +0600

    module-55 finished

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "../Cart/Cart";
+import { Link, useLoaderData } from 'react-router-dom';
+import Product from "../Product/Product";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import './Orders.css';
+import { deleteShoppingCart, removeFromDb } from "../../utilities/fakedb";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faListCheck } from '@fortawesome/free-solid-svg-icons';
 
-const Orders =() =>{
+const Orders = () => {
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+
+    const handleClearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
+
+    // console.log(savedCart);
     return (
-        <div>
-            <h2>Orders Page</h2>
+        <div className="shop-container">
+            <div className="review-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                        handleRemoveFromCart={handleRemoveFromCart}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart
+                    cart={cart}
+                    handleClearCart={handleClearCart}>
+                    <Link className="proceed-link" to="/checkout">
+                        <button className="btn-proceed"><span>Proceed Checkout </span>
+                            <FontAwesomeIcon icon={faListCheck} /></button>
+                    </Link>
+                </Cart>
+            </div>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import Home from './components/Layout/Home';
 import Orders from './components/Orders/Orders';
 import Inventory from './components/Inventory/Inventory';
 import Login from './components/Login/Login';
+import cartProductsLoader from './loaders/cartProductsLoader';
+import Checkout from './components/Checkout/Checkout';
 
 const router = createBrowserRouter([
   {
@@ -24,11 +26,16 @@ const router = createBrowserRouter([
       },
       {
         path: 'orders',
-        element: <Orders></Orders>
+        element: <Orders></Orders>,
+        loader: cartProductsLoader
       },
       {
         path: 'inventory',
         element: <Inventory></Inventory>
+      },
+      {
+        path: 'checkout',
+        element: <Checkout></Checkout>
       },
       {
         path: 'login',
